f1b2ebc1aeec4602583639735da1d4044ab825ae
Jacob Kelly <bananacreamkelly@gmail.com>
2022-02-14 17:20:24 +0100

    login branch

--- Plants-app-/seed.js
+++ Plants-app-/seed.js
@@ -5,18 +5,66 @@ mongoose.connect('mongodb://localhost/Plants-app-')
 
     const Plant = require("./models/plant.model")
 
-    const plants =  [{ 
+    const plants =  [
+        
+        { 
     Category: 'Indoor',
     family: 'Blooming',
     name: 'Spathiphyllum Wallisii',
     commonName: 'Peace Lily',
     watering: 'Once a week',
     light: 'Direct sunshine can damage the leaves - A mixture of light and shade is recommended',
+    size: 'size of plant',
     humidity: 'No particular requirements',
     temperature: 'Above 10ºC min.',
     fertilizer: 'Every 2 months',
     toxicityForPets: 'Dangerous to small children and animals',
     image_url: 'insert url'
+},
+
+    { 
+    Category: 'Indoor',
+    family: 'Fern',
+    name: 'Phlebodium Blue Star',
+    commonName: 'Blue Star Fern',
+    watering: 'Once a week',
+    light: 'Low to bright, indirect sunlight',
+    size: 'size of plant',
+    humidity: 'Prefers humid enviroment',
+    temperature: '18º-25ºC.',
+    fertilizer: 'Once a month',
+    toxicityForPets: 'Non-toxic',
+    image_url: 'insert url'
+},
+
+{ 
+    Category: 'Indoor',
+    family: 'Tropical Plants',
+    name: 'Ficus Elastica, Ficus Robusta',
+    commonName: 'Rubber Plant',
+    watering: 'Once a week',
+    light: 'medium, indirect sunlight',
+    size: 'size of plant',
+    humidity: 'Naturally prefer higher humidty, but not essentail',
+    temperature: '16-24ºC',
+    fertilizer: 'Once a month',
+    toxicityForPets: 'Dangerous to small children and animals',
+    image_url: 'insert url'
+},
+
+{ 
+    Category: 'Indoor',
+    family: 'Fern',
+    name: 'Sword Fern',
+    commonName: 'Boston Fern',
+    watering: 'Once a week',
+    light: 'Direct sunshine can damage the leaves - A mixture of light and shade is recommended',
+    size: 'size of plant',
+    humidity: 'Prefers humid enviroment',
+    temperature: '18º-25ºC',
+    fertilizer: 'Every 2 months',
+    toxicityForPets: 'Non-toxic',
+    image_url: 'insert url',
 }
 ]
 
