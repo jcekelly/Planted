9cba2372c987da35082a37ae62ed75e414ad602a
Hector <h.marfen@gmail.com>
2022-02-15 16:17:18 +0100

    add to dashboard

--- Plants-app-/routes/index.js
+++ Plants-app-/routes/index.js
@@ -10,9 +10,9 @@ router.get("/", (req, res, next) => {
 // route for user dashboard 
 
 router.get('/dashboard', (req, res, next) => {
-    User.findById(req.params.id).populate("myPlants")
-      .then(personalCollectionPlants => {
-        res.render('dashboard.hbs', {plants: personalCollectionPlants});
+    User.findById(req.session.currentUser._id).populate("myPlants")
+      .then(userFromDb => {
+        res.render('dashboard.hbs', {user: userFromDb});
       })
       .catch(error => {
         next(error);
@@ -35,11 +35,14 @@ router.get('/search-result', (req, res, next) => {
 //this is the route to add the plant to your private collection
 
 router.get('/dashboard/:id/add', (req, res, next) => {
-  User.findByIdAndUpdate(req.session.user._id, {
+  User.findByIdAndUpdate(req.session.currentUser._id , {
     $push: {myPlants: req.params.id}
   })
-    .then(newPlantInCollection => {
-      res.render('dashboard.hbs', { plant: newPlantInCollection }); 
+  //console.log('session', req.session)
+  //User.findById(req.session.currentUser._id)
+    .then((user) => {
+      console.log(user)
+     res.redirect('/dashboard')
     })
     .catch(err => {
       next(err);
@@ -59,14 +62,16 @@ router.get('/dashboard/:id/edit', (req, res, next) => {
 });
 
 
-router.post('/dashboard/:id/delete', (req, res, next) => {
-    Plant.findOneAndDelete({ _id: req.params.id })
-      .then(() => {
-        res.redirect('/dashboard');
-      })
-      .catch(err => {
-        next(err);
-      })
+router.get('/dashboard/:id/delete', (req, res, next) => {
+  User.findByIdAndUpdate(req.session.currentUser._id , {
+    $pull: {myPlants: req.params.id}
+  })
+    .then(() => {
+      res.redirect('/dashboard')
+    })
+    .catch(err => {
+      next(err);
+    })
 });
 
 module.exports = router;
