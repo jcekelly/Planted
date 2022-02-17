c4ce6cda460a4e20ce23011b42ef32aba2ffcd6c
Jacob Kelly <bananacreamkelly@gmail.com>
2022-02-17 16:00:14 +0100

    dotenv

--- seed.js
+++ seed.js
@@ -1,3 +1,4 @@
+require("dotenv/config");
 const mongoose = require('mongoose')
 mongoose.connect('process.env.MONGODB_URI')
 	.then(db => console.log(`connected to database ${db.connections[0].name}`))
